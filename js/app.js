23032b708425edd50dd90999d2b40d41eacb88c2
Errah <errah@ypdigital.com.au>
2019-08-27 17:20:19 +0800

    JS background images

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 $(function () {
+    //Mobile Menu
+    $('#button-mobile-menu').on('click', function () {
+        $('body').toggleClass('show-menu');
+        $(this).toggleClass('active');
+    })
+
+
+    //Sliders
     $('.slider-banner').slick({
         fade: true,
         dots: false,
@@ -13,4 +21,25 @@ $(function () {
         dots: true,
         infinite: true
     });
+
+
+    //Switching background images
+    $(window).on('resize', function () {
+        var bannerElement = $('.banner-switch')
+        var breakpoints = {
+            lg: 1023,
+            md: 768,
+            sm: 480
+        };
+        var wind = $(window).innerWidth();
+        var bannerLarge = $(bannerElement).data('banner-large');
+        var bannerSmall = $(bannerElement).data('banner-small');
+        if (wind > breakpoints.lg){
+            $(bannerElement).css('backgroundImage', 'url('+bannerLarge+')');
+            console.log('large');
+        }else{
+            $(bannerElement).css('backgroundImage', 'url('+bannerSmall+')');
+            console.log('small')
+        }
+    })
 })
